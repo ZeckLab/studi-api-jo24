a40c5d66a5858987cedfb8d55bc7d3fd3db0364a
ZeckLab <sturbout@gmail.com>
2024-05-10 12:06:37 +0200

    feature - Adding, updating, getting an event

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
 
+from src.api import api
+from src.core.config.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(api.router, prefix="/api")
