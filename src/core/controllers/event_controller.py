876a87fbcf7515666dc1726dadcda7c1979f83a1
ZeckLab <sturbout@gmail.com>
2024-05-13 23:26:35 +0200

    feature - Adding the error handling - 404 (not found) and 400 (already exists)

--- src/core/controllers/event_controller.py
+++ src/core/controllers/event_controller.py
@@ -13,6 +13,16 @@ async def get_by_id(db: Session, id: int) -> EventInDB:
     # Return the event in a EventInDB : model Pydantic
     return EventInDB(id=result.event_id, title=result.event_title, description=result.event_description, date=result.event_date, capacity=result.event_capacity, visible=result.event_visible)
 
+
+'''Get an event by its title in the database'''
+async def get_by_title(db: Session, title: str) -> Event | None:
+    # Get the event by its title
+    session_event = db.query(Event).filter(Event.event_title == title).first()
+    
+    # Return the event
+    return session_event
+
+
 '''Get all events in the database or only the visible events in the database'''
 async def get_all(db: Session, type: str) -> list[EventInDB]:
     result = []
@@ -42,37 +52,23 @@ async def create(db: Session, event: EventBase) -> EventInDB:
     return EventInDB(id=new_event.event_id, title=new_event.event_title, description=new_event.event_description, date=new_event.event_date, capacity=new_event.event_capacity, visible=new_event.event_visible)
 
 '''Update an event in the database'''
-async def update(event_id: int, event: EventBase, db: Session) -> EventInDB | dict:
-    # Get the event by its id
-    result = db.query(Event).filter(Event.event_id == event_id).first()
-    
-    if result is not None:
-        # Update the event if it exists
-        result.event_title = event.title
-        result.event_description = event.description
-        result.event_date = event.date
-        result.event_capacity = event.capacity
-        db.commit()
-        db.refresh(result)
-    else:
-        # Return an error message if the event does not exist
-        return JSONResponse(status_code = status.HTTP_400_BAD_REQUEST, content = {"message": "Event not found"}).__dict__
+async def update(event: Event, event_in: EventBase, db: Session) -> EventInDB:
+    # Update the event if it exists
+    event.event_title = event_in.title
+    event.event_description = event_in.description
+    event.event_date = event_in.date
+    event.event_capacity = event_in.capacity
+    db.commit()
+    db.refresh(event)
     
     # Return the event in a EventInDB : model Pydantic
-    return EventInDB(id=result.event_id, title=result.event_title, description=result.event_description, date=result.event_date, capacity=result.event_capacity, visible=result.event_visible)
+    return EventInDB(id=event.event_id, title=event.event_title, description=event.event_description, date=event.event_date, capacity=event.event_capacity, visible=event.event_visible)
 
-async def update_visibility(event_id: int, db: Session) -> EventInDB | dict:
-    #
-    result = db.query(Event).filter(Event.event_id == event_id).first()
-    
-    if result is not None:
-        # Update the event visibility
-        result.event_visible = not result.event_visible
-        db.commit()
-        db.refresh(result)
-    else:
-        # Return an error message if the event does not exist
-        return JSONResponse(status_code = status.HTTP_400_BAD_REQUEST, content = {"message": "Event not found"}).__dict__
+async def update_visibility(event:Event, db: Session) -> EventInDB | dict:
+    # Update the event visibility
+    event.event_visible = not event.event_visible
+    db.commit()
+    db.refresh(event)
     
     # Return the event in a EventInDB : model Pydantic
-    return EventInDB(id=result.event_id, title=result.event_title, description=result.event_description, date=result.event_date, capacity=result.event_capacity, visible=result.event_visible)
+    return EventInDB(id=event.event_id, title=event.event_title, description=event.event_description, date=event.event_date, capacity=event.event_capacity, visible=event.event_visible)
