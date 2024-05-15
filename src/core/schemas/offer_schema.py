ab9a5a958826e00eded83395be83fa7c6fc44a2e
ZeckLab <sturbout@gmail.com>
2024-05-15 15:46:02 +0200

    Feature - Adding the endpoint offers to create, read, update, active/desactive an offer

---
+++ src/core/schemas/offer_schema.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+# Offer Schema : Pydantic Model
+# By default, the visible attribute is not required because it has a default value in the database.
+class OfferBase(BaseModel):
+    title: str
+    description: str
+    nb_people: int = Field(gt=0)
+    price: float = Field(gt=0.0)
+    image_url: str
+
+class OfferInDB(OfferBase):
+    model_config = ConfigDict(from_attributes=True)
+    
+    offer_id: int
+    visible: bool
