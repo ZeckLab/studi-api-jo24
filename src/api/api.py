ae1137e35b9e31fd3c5a71490715cfdf9c8c7a44
ZeckLab <sturbout@gmail.com>
2024-05-15 15:46:02 +0200

    Feature - Adding the endpoint offers to create, read, update, active/desactive an offer

--- src/api/api.py
+++ src/api/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from src.api.endpoint import event_endpoint
+from src.api.endpoint import offer_endpoint
 
 router = APIRouter()
 
@@ -8,5 +9,12 @@ router.include_router(
     event_endpoint.router,
     prefix="/events",
     tags=["Event"],
+    responses={404: {"description": "Not found"}},
+)
+
+router.include_router(
+    offer_endpoint.router,
+    prefix="/offers",
+    tags=["Offer"],
     responses={404: {"description": "Not found"}},
 )
