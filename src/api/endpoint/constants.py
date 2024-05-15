6e33daa80d48c947e6d6d65394a0086978c6edba
ZeckLab <sturbout@gmail.com>
2024-05-15 15:46:02 +0200

    Feature - Adding the endpoint offers to create, read, update, active/desactive an offer

--- src/api/endpoint/constants.py
+++ src/api/endpoint/constants.py
@@ -1,3 +1,6 @@
 class ErrorCode:
     EVENT_NOT_FOUND = "Event not found"
     EVENT_TITLE_ALREADY_EXISTS = "Event title already exists"
+    
+    OFFER_NOT_FOUND = "Offer not found"
+    OFFER_TITLE_ALREADY_EXISTS = "Offer title already exists"
