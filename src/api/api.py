70c33b45724fa2da031b21936287e7b5dc503b60
Stéphanie TURBOUT <122821765+ZeckLab@users.noreply.github.com>
2024-05-15 15:50:50 +0200

    Revert "Feature - Adding the endpoint offers to create, read, update, active/desactive an offer"
    This reverts commit ab9a5a958826e00eded83395be83fa7c6fc44a2e.

--- src/api/api.py
+++ src/api/api.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
 from src.api.endpoint import event_endpoint
-from src.api.endpoint import offer_endpoint
 
 router = APIRouter()
 
@@ -9,12 +8,5 @@ router.include_router(
     event_endpoint.router,
     prefix="/events",
     tags=["Event"],
-    responses={404: {"description": "Not found"}},
-)
-
-router.include_router(
-    offer_endpoint.router,
-    prefix="/offers",
-    tags=["Offer"],
     responses={404: {"description": "Not found"}},
 )
--- src/core/config/database.py
+++ src/core/config/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
+print("DATABASE_URL",os.getenv("DATABASE_URL"))
 engine = create_engine(os.getenv("DATABASE_URL"))
 SessionLocale = sessionmaker(bind=engine, autoflush=False)
 
