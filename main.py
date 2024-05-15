c3846e3b3994bab4427dd4ec33bf44e31b48b036
Stéphanie TURBOUT <122821765+ZeckLab@users.noreply.github.com>
2024-05-15 15:50:50 +0200

    Revert "feature - Adding the error handling - 404 (not found) and 400 (already exists)"
    This reverts commit 47529ec886145ec3642d6179f3a441053d02dc3a.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from src.api import api
 from src.core.config.database import Base, engine
@@ -9,7 +8,3 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(api.router, prefix="/api")
-
-app.add_middleware(
-    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"],
-)
