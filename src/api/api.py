ddff1c482b9b3da1150e5152486043a22371cc09
ZeckLab <sturbout@gmail.com>
2024-05-21 19:17:07 +0200

    feature/Signin - Adding the user/role model, the user/role schema, the user/role controller, the user/role endpoint and the authenticate endpoint to sign in a user

--- src/api/api.py
+++ src/api/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from src.api.endpoint import event_endpoint
+from src.api.endpoint import authenticate_endpoint, event_endpoint, user_endpoint
 from src.api.endpoint import offer_endpoint
 
 router = APIRouter()
@@ -16,5 +16,19 @@ router.include_router(
     offer_endpoint.router,
     prefix="/offers",
     tags=["Offer"],
+    responses={404: {"description": "Not found"}},
+)
+
+router.include_router(
+    user_endpoint.router,
+    prefix="/users",
+    tags=["User"],
+    responses={404: {"description": "Not found"}},
+)
+
+router.include_router(
+    authenticate_endpoint.router,
+    prefix="",
+    tags=["Login/Signup"],
     responses={404: {"description": "Not found"}},
 )
