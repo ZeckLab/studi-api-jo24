c1deb8b79fd98cbc0e1277205081c46173a7e581
ZeckLab <sturbout@gmail.com>
2024-05-29 19:01:31 +0200

    Add a get request to get all orders by user connected --> Send an order with a ticket and the offers associated + the quantity. Add new schemas in TIcket, ORder and Offer.

--- src/api/endpoint/order_endpoint.py
+++ src/api/endpoint/order_endpoint.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from src.api.endpoint.authenticate_endpoint import get_current_user
-from src.core.schemas.order_schema import OrderInDB, OrderCreate
+from src.core.models.User import Role
+from src.core.schemas.order_schema import OrderInDB, OrderCreate, OrderViewUser
 import src.core.controllers.order_controller as order_controller
 from src.core.config.database import get_db
 from src.core.config.security import authentication_mode
@@ -12,8 +13,31 @@ from src.core.schemas.ticket_schema import TicketPublic
 router = APIRouter()
 
 '''Create a new order'''
-@router.post("", dependencies=[Depends(authentication_mode)], response_model=TicketPublic)
-async def create_offer(offer_in: OrderCreate, db=Depends(get_db), user= Depends(get_current_user)):       
+@router.post("", dependencies=[Depends(authentication_mode)], response_model=OrderInDB)
+async def create_order(offer_in: OrderCreate, db=Depends(get_db), user= Depends(get_current_user)):
+    '''Create a new order
+    :param offer_in: the order to create
+    :param db: the database session
+    :param user: the user who creates the order
+    :return: the order created in the database'''
+    
     # Create the order
-    ticket_public : TicketPublic = await order_controller.create(db, offer_in, user)
-    return ticket_public
+    order_in_db : OrderInDB = await order_controller.create(db, offer_in, user)
+    return order_in_db
+
+
+'''Get all orders of the current user with the details of the order'''
+@router.get("", dependencies=[Depends(authentication_mode)], response_model=list[OrderViewUser])
+async def get_orders(db: Session = Depends(get_db), user= Depends(get_current_user)):
+    '''Get all orders of the current user with the details of the order
+    :param db: the database session
+    :param user: the user
+    :return: a list of orders with the details of the order and the ticket associated
+    '''
+    
+    if(user is None):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
+    orders = await order_controller.get_all_for_user(db, user)
+    
+    return orders
--- src/core/schemas/order_schema.py
+++ src/core/schemas/order_schema.py
@@ -1,28 +1,40 @@
+from datetime import datetime
 from pydantic import BaseModel, ConfigDict
 
-from src.core.schemas.ticket_schema import OffersTicketCreate
+from src.core.schemas.ticket_schema import OffersTicketCreate, OffersTicketView, TicketPublic
 
 class OrderBase(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     
     name : str
-    date_time : str
+    date_time : datetime
 
+# A schema to validate a payment during the creation of an order
 class Payment(BaseModel):
     card_number : str
     card_expiry : str
     card_cvc : str
     mount : float
 
+# A schema to validate a element of the cart
+# during the creation of an order
 class CartItem(BaseModel):
     offer_name : str
     quantity : int
 
+# A schema to validate the creation of an order
 class OrderCreate(BaseModel):
     cart : list[CartItem]
     payment : Payment
     nb_people : int
-    
+
+# A schema to view an order with the ticket associated
+# and the information of the offers associated to the ticket
+class OrderViewUser(OrderBase):
+    ticket: TicketPublic
+    details: list[OffersTicketView]
+
+
 class OrderInDB(OrderBase):
     order_id : int
     user_id : int
